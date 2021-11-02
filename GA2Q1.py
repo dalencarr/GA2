7cbc435a61fa3c6a8a8b931581920d13195c8a7f
dalencarr <30486095+dalencarr@users.noreply.github.com>
2021-11-01 20:25:47 -0400

    Update GA2Q1.py

--- GA2Q1.py
+++ GA2Q1.py
@@ -2,16 +2,22 @@
 # Matias Espinoza -- mespinoza2@student.gsu.edu
 # Elizabeth Poythress–epoythress3@student.gsu.edu
 
-# ask for password
-pw = input("Please enter a password: ")
-# check password length, make sure it contains letters and numbers
-if 10 < len(pw) < 16 and pw.isalnum() and not pw.isalpha() and not pw.isnumeric():
-    # verify password
-    pw_check = input("Re-enter password: ")
-    if pw == pw_check:
-        print("Password created.")
-    else:
-        print("Passwords do not match.")
 
-else:
-    print("Your password should contain 10 to 15 characters and should be only numbers and letters.")
+# create initial condition so that program will only run when user is not verified
+verified = False
+
+while not verified:
+    # ask for password
+    pw = input("Please enter a password: ")
+    # check password length, make sure it contains letters and numbers
+    if 10 < len(pw) < 16 and pw.isalnum() and not pw.isalpha() and not pw.isnumeric():
+        # verify password
+        pw_check = input("Re-enter password: ")
+        if pw == pw_check:
+            print("Password created.")
+            verified = True
+        else:
+            print("Passwords do not match.")
+
+    else:
+        print("Your password should contain 10 to 15 characters and should be only numbers and letters.")
